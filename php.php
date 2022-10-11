5070b7f43ea5bcfbc155a5b0f12b1a27cc6e36ab
FagnerPontes <67767807+FagnerPontes@users.noreply.github.com>
2022-10-11 13:14:13 -0300

    Update php.php

--- php.php
+++ php.php
@@ -1,3 +1,19 @@
 <?php
-echo "<p>Copyright &copy; 1999-" . date("Y") . " W3Schools.com</p>";
+// Meu arquivo
+$arquivo = 'file.txt';
+
+// Verifica se o arquivo existe
+if ( file_exists( $arquivo ) ) {
+	// Cria o recurso (abrir o arquivo)
+	$handle = fopen( $arquivo, 'r' );
+
+	// Lê o arquivo (se existir) 
+	$ler = fread( $handle, filesize($arquivo) );
+
+	// Mostra dados na tela
+	echo $ler;
+
+	// Fecha o arquivo
+	fclose($handle);
+}
 ?>
